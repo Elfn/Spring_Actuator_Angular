c02a60cdfb07b55f055a9d971a468669f6fcdb86
Elimane Fofana <elimane.fofana@supinfo.com>
2021-05-18 02:03:25 +0000

    new

--- dashboard-ADMIN/src/app/app.module.ts
+++ dashboard-ADMIN/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {AdminDashboardService} from './services/admin-dashboard.service';
 import {HttpClientModule} from '@angular/common/http';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
